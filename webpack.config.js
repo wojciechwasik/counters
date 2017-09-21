9322414ef78bc9bf0133cd1a4a23474ef56e8753
Wojciech Wasik <wojciech.wasik@ocado.com>
2017-09-21 17:40:57 +0200

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.es6',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { presets: ['es2015', 'stage-0'] }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+    target: 'web'
+};
