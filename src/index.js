9465b86c8787297f08e320c54b8834267e77a32c
Wojciech Wasik <wojciech.wasik@ocado.com>
2017-09-21 14:49:05 +0200

    starter sources for react tutorial

---
+++ src/index.js
@@ -0,0 +1,132 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+require('./index.css');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Square = function (_React$Component) {
+  _inherits(Square, _React$Component);
+
+  function Square() {
+    _classCallCheck(this, Square);
+
+    return _possibleConstructorReturn(this, (Square.__proto__ || Object.getPrototypeOf(Square)).apply(this, arguments));
+  }
+
+  _createClass(Square, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement('button', { className: 'square' });
+    }
+  }]);
+
+  return Square;
+}(_react2.default.Component);
+
+var Board = function (_React$Component2) {
+  _inherits(Board, _React$Component2);
+
+  function Board() {
+    _classCallCheck(this, Board);
+
+    return _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).apply(this, arguments));
+  }
+
+  _createClass(Board, [{
+    key: 'renderSquare',
+    value: function renderSquare(i) {
+      return _react2.default.createElement(Square, null);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var status = 'Next player: X';
+
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'div',
+          { className: 'status' },
+          status
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'board-row' },
+          this.renderSquare(0),
+          this.renderSquare(1),
+          this.renderSquare(2)
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'board-row' },
+          this.renderSquare(3),
+          this.renderSquare(4),
+          this.renderSquare(5)
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'board-row' },
+          this.renderSquare(6),
+          this.renderSquare(7),
+          this.renderSquare(8)
+        )
+      );
+    }
+  }]);
+
+  return Board;
+}(_react2.default.Component);
+
+var Game = function (_React$Component3) {
+  _inherits(Game, _React$Component3);
+
+  function Game() {
+    _classCallCheck(this, Game);
+
+    return _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).apply(this, arguments));
+  }
+
+  _createClass(Game, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { className: 'game' },
+        _react2.default.createElement(
+          'div',
+          { className: 'game-board' },
+          _react2.default.createElement(Board, null)
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'game-info' },
+          _react2.default.createElement('div', null),
+          _react2.default.createElement('ol', null)
+        )
+      );
+    }
+  }]);
+
+  return Game;
+}(_react2.default.Component);
+
+// ========================================
+
+_reactDom2.default.render(_react2.default.createElement(Game, null), document.getElementById('root'));
